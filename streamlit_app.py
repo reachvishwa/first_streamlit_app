5126c41569a8f27e6b8d125923354042190fb1bb
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-26 16:15:34 +0100

    Update streamlit_app.py
    added  Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit;
 streamlit.title('My parents New Healthy Diner');
 streamlit.header('Breakfast Menu');
-streamlit.text('Omega3 & Blueberry Oatmeal');
-streamlit.text('Kale, Spinach and Rocket Smoothie');
-streamlit.text('Hard boiled Freerange Egg');
+streamlit.text('🥗Omega3 & 🥣 Blueberry 🥗Oatmeal');
+streamlit.text('🥑Kale, 🍞Spinach and 🍞Rocket Smoothie');
+streamlit.text('🐔Hard boiled Freerange Egg');
