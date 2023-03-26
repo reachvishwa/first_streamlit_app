6f079e4901f61a82e51cba224ff779da53148bb8
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-26 18:59:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index));
 
 # display the data frame
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list);
 
-# Display the table on the page.
 
