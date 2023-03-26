cf1bf9efa369768baacf0b5387cbf628ab91a4aa
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-26 16:12:13 +0100

    Update streamlit_app.py
    created healthy breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit;
 streamlit.title('My parents New Healthy Diner');
-streamlit.header('Breakfast menu');
-streamlit.text('Omega3 & blueberry oatmeal');
-streamlit.text('Kale, Spinach and rocket smoothie');
-streamlit.text('Hard boiled freerange Egg');
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega3 & Blueberry Oatmeal');
+streamlit.text('Kale, Spinach and Rocket Smoothie');
+streamlit.text('Hard boiled Freerange Egg');
