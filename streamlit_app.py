2f005efbf4d2b266299f5f78ed007dfe2e13dd57
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-26 16:11:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit;
 streamlit.title('My parents New Healthy Diner');
+streamlit.header('Breakfast menu');
+streamlit.text('Omega3 & blueberry oatmeal');
+streamlit.text('Kale, Spinach and rocket smoothie');
+streamlit.text('Hard boiled freerange Egg');
