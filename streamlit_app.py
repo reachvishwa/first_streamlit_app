d8bd31aa8e7dff081ca91d242deaa7437ed03793
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-27 18:47:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,4 @@ streamlit.text(fruityvice_response);
 
 streamlit.header("Fruityvice Fruit Advice!");
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
-streamlit.text(fruityvice_response.jason());
+streamlit.text(fruityvice_response.json());
