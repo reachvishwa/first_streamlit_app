31567f2c04a35f3eb423aa1837c6e728067fff02
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-27 19:02:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,13 +31,19 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response);
 
 streamlit.header("Fruityvice Fruit Advice!");
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests;
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi");
-streamlit.text(fruityvice_response.json());
+
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
+#streamlit.text(fruityvice_response.json());
 
 
 # write your own comment -what does the next line do? 
 # transposes the json response into a tabular form
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
 # write your own comment - what does this do?
 # displays data as a table with unique row identifier
-streamlit.dataframe(fruityvice_normalized);
+#streamlit.dataframe(fruityvice_normalized);
