62bafee2a0985ee4df6cd6e9386a15af40cccb6d
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-27 19:04:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,5 @@ streamlit.text(fruityvice_response.json());
 import requests;
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi");
 
-# write your own comment -what does the next line do? 
-# transposes the json response into a tabular form
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
-# write your own comment - what does this do?
-# displays data as a table with unique row identifier
-#streamlit.dataframe(fruityvice_normalized);
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
+streamlit.dataframe(fruityvice_normalized);
