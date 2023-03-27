0ff73a8403f94215cbcd31118210ed385648f28c
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-27 18:52:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response);
 
 streamlit.header("Fruityvice Fruit Advice!");
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all");
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all;
 streamlit.text(fruityvice_response.json());
 
 
 # write your own comment -what does the next line do? 
+# transposes the json response into a tabular form
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized);
