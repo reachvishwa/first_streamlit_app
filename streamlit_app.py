a5ceb7fd2c5493859adb015df12532e692594669
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-28 07:38:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,5 @@ my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.FRUITYVICE");
 # my_cur.execute(" select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST ");
 my_data_row = my_cur.fetchone();
 streamlit.text("Fruit Load List Contains:");
-streamlit.text(my_data_row);
+#streamlit.text(my_data_row);
+streamlit.dataframe(my_data_row);
