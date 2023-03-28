eddc21d590103d760d236523bc5f74a83859b02d
NATARAJAN VISWANATHAN <reachvishwa@gmail.com>
2023-03-28 07:10:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit;
 import pandas;
 import requests;
-import snowflake.connector;
+
 streamlit.title('My parents New Healthy Diner');
 streamlit.header('Breakfast Menu');
 streamlit.text('🥗Omega3 & 🥣 Blueberry 🥗Oatmeal');
@@ -45,3 +45,12 @@ import requests;
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
 streamlit.dataframe(fruityvice_normalized);
+
+
+import snowflake.connector;
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
+my_cur = my_cnx.cursor();
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()");
+my_data_row = my_cur.fetchone();
+streamlit.text("Hello from Snowflake:");
+streamlit.text(my_data_row);
